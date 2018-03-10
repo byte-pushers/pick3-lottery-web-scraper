22184f9dc3d386f20c852bcf13e8d4300d764f5d
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-03-10 08:30:09 -0600

    Revert file back to original code.

--- src/main/javascript/software.bytepushers.pick3.lottery.web.scraper.js
+++ src/main/javascript/software.bytepushers.pick3.lottery.web.scraper.js
@@ -6,42 +6,6 @@
 var cheerio = require("cheerio");
 var request = require("request");
 
-console.log("Grabbing table data from Pick 3");
-
-var url = "http://www.lotteryusa.com/texas/evening-pick-3/"
-request(url, function(err,resp,html){
-
-    var $ = cheerio.load(html);
-
-    var results = [];
-
-    $("tbody").each(function(i, element){
-    /*Pulled out the Dates, now need to separate them
-    */
-
-        var date = $(".date");
-        var dateText = date.text();
-
-         // var drawingTime = $();
-   /* Pulled out the winnig numbers , Now the numbers need to be parsed into a format
-   in which they are separated in threes.
-   */
-         var win_numbers = $(".draw-result").children().text();
-
-
-        results.push({
-            date: dateText,
-            // drawingTime: drawingTime,
-            win_numbers: win_numbers
-        });
-    });
-    console.log(results);
-});
-
-
-
-
-/*
 (function () {
     'use strict';
     var request = require('request');
@@ -56,7 +20,7 @@ request(url, function(err,resp,html){
 
     BytePushers.service.Scraper.scrapeTxPick3LotterySite = function (error, response, html) {
         if (!error) {
-            var $ = cheerio.load(html,
+            var $ = cheerio.load(html),
                 date,
                 drawingTime,
                 number;
@@ -67,7 +31,6 @@ request(url, function(err,resp,html){
     };
 
 }());
-*/
 
 /*function scrapeTxPick3LotterySite = function (error, response, html) {
     if (!error) {
