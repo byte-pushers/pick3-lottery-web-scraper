898ab25eab87ba19944751cca74c8a90ca6fc082
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-03-12 10:25:46 -0500

    broke test to see if email is sent out on build failure.

--- src/test/javascript/newline.replaceregex.js
+++ src/test/javascript/newline.replaceregex.js
@@ -6,6 +6,6 @@ describe("Regular Express Tests", function() {
             expectedText = "8 ",
             actualText = text.replace(/\r|\n/g, "");
 
-        assert(actualText, expectedText);
+        assert.equal(actualText, expectedText);
     });
 });
