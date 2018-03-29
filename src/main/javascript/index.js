901b400d2223c69fec869ad07d6e28d3be9569e3
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-03-29 10:35:41 -0500

    Re-factored where AWS Lambda handler is exported.

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -7,3 +7,19 @@ BytePushers.WebScraper = require('./software.bytepushers.pick3.lottery.web.WebSc
 BytePushers.TexasPick3WebScraper = require('./software.bytepushers.pick3.lottery.web.TexasPick3WebScraper');
 
 module.exports = BytePushers;
+module.exports.retrieveWinningNumber = function(event, context, callback) {
+    var drawingState = (event && event.drawingState)? event.drawingState: null;
+    var drawingDate = (event && event.drawingDate)? new Date(event.drawingDate): null;
+    var drawingTime = (event && event.drawingTime)? event.drawingTime: null;
+    var service = new BytePushers.Pick3LotteryWebScrapingService();
+
+    if (drawingState && drawingDate && drawingTime) {
+        return service.retrieveWinningNumber(drawingState, drawingDate, drawingTime).then(function (actualMorningWinningNumber) {
+            callback(null, actualMorningWinningNumber.number);
+        }, function (error) {
+            callback(error);
+        });
+    } else {
+        callback("Required parameters must be defined.");
+    }
+};
