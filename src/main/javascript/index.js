f7f6e14b544b9f55ff40800a73bf3c3d8337be83
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-04-06 13:53:38 -0500

    Debugging AWS Lambda invocation.

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -1,25 +1,33 @@
 /*global require, module*/
 
-var BytePushers = {};
+/*var BytePushers = {};
 BytePushers.opp = require('bytepushers-js-oop');
 BytePushers.Pick3LotteryWebScrapingService = require('./software.bytepushers.pick3.lottery.web.Pick3LotteryWebScrapingService');
 BytePushers.WebScraper = require('./software.bytepushers.pick3.lottery.web.WebScraper');
 BytePushers.TexasPick3WebScraper = require('./software.bytepushers.pick3.lottery.web.TexasPick3WebScraper');
 
-module.exports = BytePushers;
+module.exports = BytePushers;*/
 module.exports.retrieveWinningNumber = function(event, context, callback) {
-    var drawingState = (event && event.drawingState)? event.drawingState: null;
-    var drawingDate = (event && event.drawingDate)? new Date(event.drawingDate): null;
-    var drawingTime = (event && event.drawingTime)? event.drawingTime: null;
-    var service = new BytePushers.Pick3LotteryWebScrapingService();
-
-    if (drawingState && drawingDate && drawingTime) {
-        return service.retrieveWinningNumber(drawingState, drawingDate, drawingTime).then(function (actualMorningWinningNumber) {
-            callback(null, actualMorningWinningNumber.number);
-        }, function (error) {
-            callback(error);
-        });
-    } else {
-        callback("Required parameters must be defined.");
-    }
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello World!",
+            input: event
+        })
+    };
+    callback(null, response);
+    // var drawingState = (event && event.drawingState)? event.drawingState: null;
+    // var drawingDate = (event && event.drawingDate)? new Date(event.drawingDate): null;
+    // var drawingTime = (event && event.drawingTime)? event.drawingTime: null;
+    // var service = new BytePushers.Pick3LotteryWebScrapingService();
+    //
+    // if (drawingState && drawingDate && drawingTime) {
+    //     return service.retrieveWinningNumber(drawingState, drawingDate, drawingTime).then(function (actualMorningWinningNumber) {
+    //         callback(null, actualMorningWinningNumber.number);
+    //     }, function (error) {
+    //         callback(error);
+    //     });
+    // } else {
+    //     callback("Required parameters must be defined.");
+    // }
 };
