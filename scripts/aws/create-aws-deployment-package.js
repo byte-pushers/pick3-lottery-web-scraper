2107e49a17e54bf82ea0064e467375bf1b56f811
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-04-08 19:11:22 -0500

    Working on getting all module dependencies and package.json files in build/node_module folder.

--- scripts/aws/create-aws-deployment-package.js
+++ scripts/aws/create-aws-deployment-package.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var AdmZip = require('adm-zip');
 var datetime = require('node-datetime');
 var shell = require('shelljs');
+var dependencyTree = require('dependency-tree');
 var TRAVIS_BUILD_DIR = (process.argv[4] !== undefined && process.argv[4] !== null)? process.argv[4] : ".";
 
 function deleteFolderRecursive(path) {
@@ -90,6 +91,8 @@ function createAwsDeploymentPackage(awsDeploymentSourcePackagePath) {
         }
     });
 
+    getRequestModuleDependencies();
+
     shell.cd(TRAVIS_BUILD_DIR + '/build');
     shell.exec("zip -r " + getAwsDeploymentPackagePath() + " . -x \"*.DS_Store\"");
 }
@@ -122,6 +125,68 @@ function printOutWorkspaceInfo(workspacePath) {
         console.log("TRAVIS_BUILD_DIR is: " + TRAVIS_BUILD_DIR);
         printDirectoryInfo(TRAVIS_BUILD_DIR);
     }
+}
+
+function getRelativePath(sourcePath, relativeSearchCriteria) {
+    var relativeSearchCriteriaIndex = -1, relativePath;
+
+    if (sourcePath !== null && sourcePath !== undefined && relativeSearchCriteria !== null && relativeSearchCriteria !== undefined) {
+        relativeSearchCriteriaIndex = sourcePath.indexOf(relativeSearchCriteria);
+
+        if (relativeSearchCriteriaIndex > -1) {
+            relativePath = sourcePath.substring(relativeSearchCriteriaIndex);
+        }
+    }
+
+    return relativePath;
+}
+
+function ensureTargetPathExist(targetPath) {
+    var targetPaths = (targetPath)? targetPath.split("/"): [];
+    var relativePath = "";
+    var relativePathPrefix = TRAVIS_BUILD_DIR + "/build";
+
+    targetPaths.forEach(function (path, pathIndex) {
+        if (path) {
+            if (path.trim().length > 0) {
+                if (pathIndex < targetPaths.length -1) {
+                    relativePath += "/" + path;
+
+                    if (pathIndex === 2) {
+                        var sourcePackageJsonPath = TRAVIS_BUILD_DIR + relativePath + "/package.json";
+                        var targetPackageJsonPath = relativePathPrefix + relativePath + "/package.json";
+
+                        if (fs.existsSync(sourcePackageJsonPath)) {
+                            shell.exec("cp " + sourcePackageJsonPath + " " + targetPackageJsonPath);
+                        }
+                    }
+
+                    if (!fs.existsSync(relativePathPrefix + relativePath)) {
+                        fs.mkdirSync(relativePathPrefix + relativePath);
+                    }
+                }
+            }
+        }
+    });
+}
+
+function getRequestModuleDependencies() {
+    var dependencies = dependencyTree.toList({
+        filename: TRAVIS_BUILD_DIR + "/node_modules/request/index.js",
+        directory: TRAVIS_BUILD_DIR + "/node_modules/request",
+
+    });
+
+    dependencies.forEach(function (dependencySourcePath, dependencyIndex) {
+        var dependencyTargetPath = getRelativePath(dependencySourcePath, "/node_modules");
+        var copyCommand = "cp " + dependencySourcePath + " " + TRAVIS_BUILD_DIR + "/build" + dependencyTargetPath;
+
+        console.log("dependency["+dependencyIndex+"]: " + dependencySourcePath);
+        console.log("copy command: " + copyCommand);
+
+        ensureTargetPathExist(dependencyTargetPath);
+        shell.exec(copyCommand);
+    });
 }
 
 clean();
