dced9d57e41d5f0828ba5a6e8511a901467e73fb
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-08-15 14:18:06 -0500

    Update TypeScript declaration file for service to be accurate.

--- src/main/javascript/software.bytepushers.pick3.lottery.web.Pick3LotteryWebScrapingService.d.ts
+++ src/main/javascript/software.bytepushers.pick3.lottery.web.Pick3LotteryWebScrapingService.d.ts
@@ -1,12 +1,11 @@
 declare module MPick3LotteryWebScrapingService {
     export interface Pick3LotteryWebScrapingService {
-        toString():string;
-        new(drawingTime:string, drawingDat:Date):Pick3LotteryWebScrapingService;
+        new():Pick3LotteryWebScrapingService;
     }
 
     export interface Main {
         retrieveWinningNumber(drawingState:string, drawingDate:Date, drawingTime:string):Promise;
-        new(drawingTime:string, drawingDat:Date):Pick3LotteryWebScrapingService;
+        new():Pick3LotteryWebScrapingService;
     }
 }
 
